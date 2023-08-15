520d42f5913ce0f64339f958cfd5b3b879a2a76d
TheLongFace <coco.28tt@gmail.com>
2023-08-15 13:26:13 +0900

    Initial Commit

---
+++ ConnectionTest/ConnectionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectionTestApp.swift
+//  ConnectionTest
+//
+//  Created by Takafumi Shimizu on 2023/08/15.
+//
+
+import SwiftUI
+
+@main
+struct ConnectionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
